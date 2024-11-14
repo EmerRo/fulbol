274eb83f89dc13707c8a04f55588c8a3602c130c
EmerRo <yarlequerodrigo9@gmail.com>
2024-11-14 14:20:21 -0500

    cambio1s

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'; // Si estás usando Vue, o puedes omitirlo si no lo usas
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [vue()], // Solo si estás usando Vue
-  build: {
-    outDir: 'public/build', // Especifica que los archivos de producción deben guardarse en public/build
-    manifest: true, // Asegúrate de que el manifest.json se genere
-  },
-  server: {
-    proxy: {
-      '/app': 'http://localhost',
-    },
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
