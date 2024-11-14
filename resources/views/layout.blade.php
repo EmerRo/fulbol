f3997ddad7226728c71b9fc8b7413040f05ffeb9
EmerRo <yarlequerodrigo9@gmail.com>
2024-11-14 15:02:30 -0500

    cambio25

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,13 +7,6 @@
     <title>@yield('title', 'Full Sports | Vive tu pasión por el deporte')</title>
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @production
-        @php
-            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-        @endphp
-        <script type="module" src="/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>
-        <link rel="stylesheet" href="/build/{{ $manifest['resources/css/app.css']['file'] }}">
-    @endproduction
 </head>
 <body>
     @include('header')
