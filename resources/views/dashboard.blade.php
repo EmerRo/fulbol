60e50c722cda787be6c82c784d89772e0576dbfa
EmerRo <yarlequerodrigo9@gmail.com>
2024-11-14 13:56:00 -0500

    holaDf

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,5 @@
 @extends('layouts.app')
- 
-@production
-        @php
-            $manifestPath = public_path('build/manifest.json');
-            $manifest = json_decode(file_get_contents($manifestPath), true);
-        @endphp
-        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
-        <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
-    @else
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endproduction
+
 @section('content')
     @include('sections.header')
     <div class="flex">
